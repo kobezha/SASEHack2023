321abea753bf0ff5b4125c1a841f8338fddd56d9
andrewwchong <andrewwchong@gmail.com>
2023-10-08 23:13:26 -0400

    accidentally made bad change, reverting to before 11PM

--- SocialNetwork-webapp/webapps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, re_path
-from socialnetwork import views
-
-urlpatterns = [
-    re_path(r'^$',              views.global_stream),
-    re_path(r'^socialnetwork/', include('socialnetwork.urls')),
-]
